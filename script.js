e87e66577db637a55ef630a2ea0ee1d5b7120daa
bourayaksel-spec <bourayaksel@gmail.com>
2026-08-11 22:12:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,22 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+document.addEventListener("DOMContentLoaded", function () {
 
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    if (menuToggle && navLinks) {
+
+        menuToggle.addEventListener("click", function () {
+
+            navLinks.classList.toggle("active");
+
+        });
+
+    }
+
+});
 
 
 
