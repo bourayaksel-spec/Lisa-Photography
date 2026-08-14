7473f60791e9247b8f7556c7af5df6fb8f2fcb65
bourayaksel-spec <bourayaksel@gmail.com>
2026-08-14 19:26:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -407,27 +407,23 @@ filterButtons.forEach(button => {
 
         const filter = button.dataset.filter;
 
-        // Remove active class
         filterButtons.forEach(btn => {
             btn.classList.remove("active");
         });
 
-        // Add active class
         button.classList.add("active");
 
-        // Filter images
         galleryItems.forEach(item => {
 
-            const category = item.dataset.category;
+            const categories = item.dataset.category.split(" ");
 
-            if (filter === "all" || category === filter) {
-
+            if (
+                filter === "all" ||
+                categories.includes(filter)
+            ) {
                 item.classList.remove("hidden");
-
             } else {
-
                 item.classList.add("hidden");
-
             }
 
         });
